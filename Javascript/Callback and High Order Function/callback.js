b80ffa3eefe3ba8fba2b761bc4e6ce142aeef3a2
Prerit Nagpal <137195896+preritnagpal@users.noreply.github.com>
2026-09-22 21:51:45 +0530

    Add files via upload

--- Javascript/Callback and High Order Function/callback.js
+++ Javascript/Callback and High Order Function/callback.js
@@ -1,1 +1,23 @@
+function operate(num1, num2, operation){
+    return operation(num1, num2);
+}
+function add(a,b){
+    return a+b;
+}
+console.log(operate(5,4,add));
 
+// Arrow
+
+const operate2 = (num1, num2, operation) => {
+    return operation(num1,num2);
+}
+const add2 = (a,b) => {
+    return a+b;
+}
+console.log(operate(10,14,add2));
+
+//Arrow in one line
+
+const operate3 = (num1, num2, operation) => operation(num1, num2);
+const add3 = (a,b) => a+b;
+console.log(operate3(10,14,add3));

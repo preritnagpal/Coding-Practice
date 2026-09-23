5169e265d6f831b56ac2b2eb4cd1908e1d6dd43a
Prerit Nagpal <137195896+preritnagpal@users.noreply.github.com>
2026-09-23 21:51:54 +0530

    Add files via upload

--- Javascript/Event Loop/eventloop.js
+++ Javascript/Event Loop/eventloop.js
@@ -1,1 +1,9 @@
-
+console.log("A");
+setTimeout(()=>{        // setTimeout is a Macrotask
+    console.log("B");
+},0);
+Promise.resolve()       // Promise is a Microtask
+.then(()=>{
+    console.log("C");
+});
+console.log("D");

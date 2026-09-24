0e43c7c04dff5c1a360003bf1d5d35612b063bad
Prerit Nagpal <137195896+preritnagpal@users.noreply.github.com>
2026-09-24 13:10:56 +0530

    Add files via upload

--- Javascript/Shallow and Deep Copy/deepndshallow.js
+++ Javascript/Shallow and Deep Copy/deepndshallow.js
@@ -1,1 +1,31 @@
+// Shallow Copy
 
+let user = {
+    name:"Prerit",
+    adrdess:{
+        city:"Bareilly",
+        state:"UP"
+    }
+};
+
+let shallow = {...user}
+
+shallow.adrdess.city = "Noida";
+
+console.log(user);
+
+// Deep Copy
+
+let user2 = {
+    name:"Prerit",
+    address:{
+        city:"Bareilly",
+        state:"UP"
+    }
+};
+
+let deep = structuredClone(user2)
+
+deep.address.city = "Noida";
+
+console.log(user2);

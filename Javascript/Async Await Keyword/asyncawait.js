80b8f4c877be07c3c3fb2d97407daf771b337404
Prerit Nagpal <137195896+preritnagpal@users.noreply.github.com>
2026-09-26 19:09:01 +0530

    Add files via upload

--- Javascript/Async Await Keyword/asyncawait.js
+++ Javascript/Async Await Keyword/asyncawait.js
@@ -1,1 +1,12 @@
-
+function getUser(){
+    return new Promise((resolve)=>{
+        setTimeout(()=>{
+            resolve("Api Solved");
+        },2000);
+    });
+}
+async function showData(){
+    const data = await getUser();
+    console.log(data);
+}
+showData();

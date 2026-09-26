5520c2ef59516ef19fbb8e1e72cef9c6ddfb5ad6
Prerit Nagpal <137195896+preritnagpal@users.noreply.github.com>
2026-09-26 19:10:51 +0530

    Add files via upload

--- Javascript/Callback Hell Problem Solved using Promise and Async Await/callbackhell.js
+++ Javascript/Callback Hell Problem Solved using Promise and Async Await/callbackhell.js
@@ -1,1 +1,41 @@
+// Callback Hell Problem
 
+setTimeout(()=>{
+    console.log("Print A");
+    setTimeout(()=>{
+        console.log("Print B");
+        setTimeout(()=>{
+            console.log("Print C");
+            setTimeout(()=>{
+            console.log("Print D");
+            },2000);
+        },2000);
+    },2000);
+},2000);
+
+// Callback solution Using Promise
+
+function print(letter){
+    return new Promise((resolve)=>{
+        setTimeout(()=>{
+            console.log(letter);
+            resolve();
+        },2000);
+    });
+}
+
+
+print("A")
+.then(() => print("B"))
+.then(() => print("C"))
+.then(() => print("D"));
+
+// Callback Solution Using Async/await
+
+async function printAll(){
+    await print("A");
+    await print("B");
+    await print("C");
+    await print("D");
+};
+printAll();
